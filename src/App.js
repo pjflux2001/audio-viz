7be93de339187a8bc5475fb1d3498aa7e7dff86d
Prashant <prashant.bits01@gmail.com>
2021-12-12 18:08:47 +0530

    final

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <p style={{textAlign: '-webkit-center', margin: '3 3 3 3'}}>
+      Reload page to randomize <b>song</b> and <b>image</b>
+      <br />
+      Click / Double Click on Canvas to <i>start</i> and <i>stop</i> the music
+    </p>
+    </>
+  );
+}
+
+export default App;
